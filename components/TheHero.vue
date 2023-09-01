94ee50b2e51a91a48655c13f67d89e513cf1a646
jman <90673447+kwamdo@users.noreply.github.com>
2023-09-01 22:48:20 +0300

    coding carousel 💻

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative">
+    <div class="relative text-white">
         <!-- Add a dark overlay -->
         <div class="absolute inset-0 bg-black opacity-50 z-10"></div>
         <div class="md:hidden">
@@ -14,28 +14,50 @@
             <!-- Hambuger menu -->
             <button @click="showMobileNav = !showMobileNav"
                     class="absolute top-4 right-4 z-20 p-2 bg-white rounded-full">
-                <IconCSS name="heroicons-solid:menu-alt-3"
-                         class="text-4xl" />
+                <IconCSS name="ci:menu-alt-01"
+                         class="text-4xl text-black" />
             </button>
+
+            <!-- call to action -->
+            <div class="absolute inset-0 flex items-center justify-center flex-col z-20 gap-4">
+
+                <div class=" uppercase font-jost">
+                    | Welcome to Zaman |
+                </div>
+
+                <div class="text-[40px] text-center font-semibold leading-tight text-gray-50">
+                    Delicious Food & Wonderful Eating Experience
+                </div>
+
+                <p class="text-xl  px-10 text-center font-jost">
+                    serving food , harmony and happiness since 1999
+                </p>
+
+
+            </div>
         </div>
 
         <!-- Your carousel code -->
         <div class="relative">
-            <NuxtCarousel :autoplay="4000"
+            <NuxtCarousel :autoplay="3000"
                           :items-to-show="1"
                           :wrap-around="true"
-                          :transition="1000"
+                          :transition="2000"
                           class="h-screen relative">
                 <NuxtSlide v-for="slide in data.images"
                            :key="slide"
-                           class="h-screen">
-                    <NuxtImg :src="slide.src"
-                             class="h-full object-cover" />
+                           class="h-screen carousel__item">
+                    <!-- Zoom effect on hover -->
+                    <div class="h-full overflow-hidden">
+                        <NuxtImg :src="slide.src"
+                                 class="h-full object-cover object-top" />
+                    </div>
                 </NuxtSlide>
 
-                <template #addons> </template>
+                <template #addons></template>
             </NuxtCarousel>
         </div>
+
     </div>
 </template>
 
@@ -57,17 +79,36 @@ const images = ref(data);
 }
 
 .bg-black {
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, );
     /* Adjust the opacity as needed */
 }
 
 .opacity-50 {
-    opacity: 0.9;
+    opacity: 0.55;
     /* Adjust the opacity as needed */
 }
 
 /* Z-index to control stacking order */
 .z-10 {
     z-index: 10;
+}
+
+.carousel__slide--active {
+    opacity: 1;
+    transform: scale(1.1);
+    transition: all 10s ease-in-out;
+}
+
+.carousel__slide--prev {
+    opacity: 1;
+    transform: rotateY(-10deg);
+    transition: all 10s ease-in-out;
+
+}
+
+.carousel__slide--next {
+    opacity: 1;
+    transform: rotateY(10deg);
+    transition: all 10s ease-in-out;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="h-[200vh]">
         <TheHero />
     </div>
 </template>
