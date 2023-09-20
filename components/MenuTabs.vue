b4692cb0a9351d986e540ff8239007d71bdb03d9
jman <90673447+kwamdo@users.noreply.github.com>
2023-09-20 13:50:31 +0300

    Added Menu Tabs 👍

--- components/MenuTabs.vue
+++ components/MenuTabs.vue
@@ -1,39 +1,37 @@
 <template>
-    <div class="basis-1/3 pb-4">
-        <TheHeader :title="menu.name">
-            <template #image>
-                <NuxtImg provider="cloudinary"
-                         format="webp"
-                         loading="lazy"
-                         alt="image"
-                         sizes="sm:100vw md:50vw lg:800px"
-                         :modifiers="{ effect: 'colorize:30', color: 'black' }"
-                         src="v1694095370/2O0A8677_x3a9xc.webp"
-                         class="w-full h-full object-cover object-center" />
-            </template>
-        </TheHeader>
-        {{ slug }}
+    <div class="2xl:bg-red-700">
+        <!-- Tab buttons -->
+        <div class="tab-buttons">
+            <button v-for="(tab, index) in tabs"
+                    :key="index"
+                    @click="toggleTab(index)"
+                    :class="{ active: activeTab === index }">
+                {{ tab.name }}
+            </button>
+        </div>
 
-        <!-- meu tabs component -->
-        <MenuTabs :tabs="menu.categories[0]" />
+        <!-- Tab content with transition -->
+        <transition name="fade"
+                    mode="out-in">
+            <div :key="activeTab"
+                 class="tab-content">
+                <!-- {{ tabs[activeTab].content }} -->
+                dfd
+            </div>
+        </transition>
     </div>
 </template>
 
 <script setup>
-// get route params 
-const route = useRoute();
-const slug = ref(route.params.slug);
-// get menu items from server
-const { data } = await useFetch('/api/menu');
-const FullMenu = ref(data.value.menu);
-// get corresponding menu that has the same slug as the route params using destructuring
-const menu = ref(null);
-const { BreakfastMenu, MainMenu } = FullMenu.value;
-if (route.params.slug === 'BreakfastMenu') {
-    menu.value = BreakfastMenu;
-} else {
-    menu.value = MainMenu;
-}
+import { ref } from 'vue';
+
+// Props
+const props = defineProps({
+    tabs: {
+        type: Object,
+        required: true,
+    },
+});
 
 // State
 const activeTab = ref(0);
@@ -41,7 +39,6 @@ const activeTab = ref(0);
 // Methods
 const toggleTab = (index) => {
     activeTab.value = index;
-    console.log(activeTab.value);
 };
 </script>
 
@@ -49,6 +46,7 @@ const toggleTab = (index) => {
 /* Add your CSS for styling tabs and transitions here */
 .tab-buttons {
     display: flex;
+
 }
 
 .tab-buttons button {
