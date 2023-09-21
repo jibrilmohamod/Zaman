8d9aa9301e4a5c8f521743c3715cc7db63e162dd
jman <90673447+kwamdo@users.noreply.github.com>
2023-09-21 11:14:41 +0300

    Menu Changes ⚡

--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -1,59 +1,57 @@
 <template>
-    <div class="xl:w-[500px] border-b border-gray-400 py-2">
-        <div class="flex justify-between py-4 gap-2 ">
-            <!-- Item Image  -->
-            <div class="">
-                <NuxtImg src="v1695223477/2O0A2680_t8cqwg.webp"
-                         :alt="name"
-                         provider="cloudinary"
-                         loading="lazy"
-                         sizes="sm:100px md:50vw lg:400px"
-                         format="webp"
-                         class="object-cover w-20 h-[70px] rounded-sm shadow-md shadow-black" />
-            </div>
-            <!-- Item Name and Details  -->
-            <div class="flex flex-col gap-2 basis-3/5">
-                <!-- Item Name  -->
-                <p class="font-semibold text-[22px] tracking-wide md:text-[25px] md:font-bold">
-                    {{ name }}
-                </p>
-                <!-- Item Details  -->
-                <p class="font-jost text-[#4a4a4b] text-[14px] md:text-[16px]">
-                    {{ ingredients }}
-                </p>
-            </div>
-            <!-- Item Price  -->
-            <div class="text-[#272727] place-self-start font-jost  md:text-[16px]">
-
-                {{ `Ksh${price}` }}
-
-            </div>
-        </div>
-
+  <div class="xl:w-[490px] w-[90vw] py-4 xl:py-8 border-b border-gray-400 xl:grid">
+    <div class="flex justify-between gap-2">
+      <!-- Item Image  -->
+      <div class="">
+        <NuxtImg
+          src="v1695223477/2O0A2680_t8cqwg.webp"
+          :alt="name"
+          provider="cloudinary"
+          loading="lazy"
+          sizes="sm:100px md:50vw lg:400px"
+          format="webp"
+          class="object-cover h-[70px] rounded-sm shadow-md shadow-black"
+        />
+      </div>
+      <!-- Item Name and Details  -->
+      <div class="flex flex-col gap-2 basis-3/5">
+        <!-- Item Name  -->
+        <p
+          class="font-semibold text-[22px] tracking-wide md:text-[25px] md:font-bold text-[#DD5903]"
+        >
+          {{ name }}
+        </p>
+        <!-- Item Details  -->
+        <p class="font-jost text-[#4a4a4b] text-[14px] md:text-[16px]">
+          {{ ingredients }}
+        </p>
+      </div>
+      <!-- Item Price  -->
+      <div class="text-[#272727] font-jost md:text-[16px]">
+        Ksh<span class="text-[#DD5903]">{{ `${price}.0` }}</span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
 defineProps({
-    name: {
-        type: String,
-        default: 'Item Name'
-    },
-    ingredients: {
-        type: String,
-        default: 'Item Ingredients'
-    },
-    price: {
-
-        default: '$0.00'
-    },
-    image: {
-        type: String,
-        default: 'v1694095332/2O0A2492_xhfbyj.webp'
-    }
-})
-
-
+  name: {
+    type: String,
+    default: "Item Name",
+  },
+  ingredients: {
+    type: String,
+    default: "Item Ingredients",
+  },
+  price: {
+    default: "$0.00",
+  },
+  image: {
+    type: String,
+    default: "v1694095332/2O0A2492_xhfbyj.webp",
+  },
+});
 </script>
 
 <style lang="scss" scoped></style>
--- pages/Menu/[slug].vue
+++ pages/Menu/[slug].vue
@@ -1,59 +1,60 @@
 <template>
-    <div class="basis-1/3 pb-4">
-        <TheHeader :title="menu.name">
-            <template #image>
-                <NuxtImg provider="cloudinary"
-                         format="webp"
-                         loading="lazy"
-                         alt="image"
-                         sizes="sm:100vw md:50vw lg:1500px"
-                         :modifiers="{ effect: 'colorize:30', color: 'black' }"
-                         src="v1694095370/2O0A8677_x3a9xc.webp"
-                         class="w-full h-full object-cover object-center" />
-            </template>
-        </TheHeader>
-        <div class="container m-auto pl-6 pr-3 xl:pl-0 xl:pr-3">
-            <h4 v-if="slug === 'BreakfastMenu'"
-                class="xl:w-2/4 xl:text-4xl py-5 text-2xl uppercase xl:pb-16 xl:pt-12">
-                Energize your morning with our
-                <span class="text-[#DD5903] cursor-pointer border-button">
-                    Breakfast
-                </span>
-                where you'll find a <span class="text-[#DD5903] cursor-pointer border-button">
-                    mouthwatering
-                </span> selection of dishes to
-                jumpstart your day.
-            </h4>
-            <h4 v-else
-                class="xl:w-2/4 xl:text-4xl py-5 text-2xl uppercase xl:pb-16 xl:pt-12">
-                Indulge in the rich tapestry of flavors on our <span class="text-[#DD5903] cursor-pointer border-button">
-                    Main Menu
-                </span>, where each dish tells a unique story of <span class="text-[#DD5903] cursor-pointer border-button">
-                    culinary creativity
-                </span> and passion.
-            </h4>
-            <MenuTabs :tabs="menu.categories[0]" />
-        </div>
-
+  <div class="basis-1/3 pb-4">
+    <TheHeader :title="menu.name">
+      <template #image>
+        <NuxtImg
+          provider="cloudinary"
+          format="webp"
+          loading="lazy"
+          alt="image"
+          sizes="sm:100vw md:50vw lg:1500px"
+          :modifiers="{ effect: 'colorize:30', color: 'black' }"
+          src="v1694095370/2O0A8677_x3a9xc.webp"
+          class="w-full h-full object-cover object-center"
+        />
+      </template>
+    </TheHeader>
+    <div class="container m-auto pl-6 pr-3 xl:pl-0 xl:pr-3">
+      <h4
+        v-if="slug === 'BreakfastMenu'"
+        class="xl:w-2/4 xl:text-4xl py-5 text-2xl uppercase xl:pb-16 xl:pt-12"
+      >
+        Energize your morning with our
+        <span class="text-[#DD5903] cursor-pointer border-button"> Breakfast </span>
+        where you'll find a
+        <span class="text-[#DD5903] cursor-pointer border-button"> mouthwatering </span>
+        selection of dishes to jumpstart your day.
+      </h4>
+      <h4 v-else class="xl:w-2/4 xl:text-4xl py-5 text-2xl uppercase xl:pb-16 xl:pt-12">
+        Indulge in the rich tapestry of flavors on our
+        <span class="text-[#DD5903] cursor-pointer border-button"> Main Menu </span>,
+        where each dish tells a unique story of
+        <span class="text-[#DD5903] cursor-pointer border-button">
+          culinary creativity
+        </span>
+        and passion.
+      </h4>
+      <MenuTabs :tabs="menu.categories[0]" />
     </div>
+  </div>
 </template>
 
 <script setup>
-// get route params 
+// get route params
 const route = useRoute();
 const slug = ref(route.params.slug);
 // get menu items from server
-const { data } = await useFetch('/api/menu');
+const { data } = await useFetch("/api/menu");
 const FullMenu = ref(data.value.menu);
 // get corresponding menu that has the same slug as the route params using destructuring
 const menu = ref(null);
 const { BreakfastMenu, MainMenu, Beverages } = FullMenu.value;
-if (route.params.slug === 'BreakfastMenu') {
-    menu.value = BreakfastMenu;
-} else if (route.params.slug === 'MainMenu') {
-    menu.value = MainMenu;
-} else if (route.params.slug === 'Beverages') {
-    menu.value = Beverages;
+if (route.params.slug === "BreakfastMenu") {
+  menu.value = BreakfastMenu;
+} else if (route.params.slug === "MainMenu") {
+  menu.value = MainMenu;
+} else if (route.params.slug === "Beverages") {
+  menu.value = Beverages;
 }
 
 // State
@@ -61,22 +62,29 @@ const activeTab = ref(0);
 
 // Methods
 const toggleTab = (index) => {
-    activeTab.value = index;
-    console.log(activeTab.value);
+  activeTab.value = index;
+  console.log(activeTab.value);
 };
+
+// seo
+useSeoMeta({
+  title: `${slug.value} | Zaman Restaurant`,
+  description: "Zaman Restaurant Menu",
+  image: "v1694095370/2O0A8677_x3a9xc.webp",
+  url: "https://zaman-restaurant.vercel.app/menu",
+});
 </script>
 
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
-    transition: opacity 0.5s;
+  transition: opacity 0.5s;
 }
 
 .fade-enter,
 .fade-leave-to
 
-/* .fade-leave-active in <2.1.8 */
-    {
-    opacity: 0;
+/* .fade-leave-active in <2.1.8 */ {
+  opacity: 0;
 }
 </style>
