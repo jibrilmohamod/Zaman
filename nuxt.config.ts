d3f722684487fc25d1cd66c0f7f5a05a10d78587
Jibril Mohamud <jibrilmohamod@gmail.com>
2023-12-29 13:39:03 +0300

    Made Changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { resolve } from "path"
+import { resolve } from "path";
 export default defineNuxtConfig({
- alias: {
-  "@": resolve(__dirname, "/"),
- },
- devtools: { enabled: true },
- css: ["animate.css/animate.min.css"],
- //  Nuxt image options for cloudinary
- image: {
-  cloudinary: {
-   baseURL: "https://res.cloudinary.com/dckmlqzgd/image/upload/",
+  alias: {
+    "@": resolve(__dirname, "/"),
   },
-  format: ["webp"],
-  loading: "lazy",
- },
+  devtools: { enabled: true },
 
- site: {
-  url: "https://zamann.netlify.app",
-  name: "Zaman Restaurant Nairobi",
-  description: "Zaman Restaurant Nairobi",
-  defaultLocale: "en",
-  identity: {
-   type: "Person",
-  },
-  twitter: "@mohamodjibril",
-  trailingSlash: true,
- },
- modules: [
-  "@nuxt/image",
-  "nuxt-icon",
-  "@formkit/nuxt",
-  "@nuxtseo/module",
-  "@nuxt/ui",
- ],
+  css: ["~/assets/css/main.css", "animate.css/animate.min.css"],
 
- app: {
-  pageTransition: { name: "page", mode: "out-in" },
-  head: {
-   charset: "utf-8",
-   viewport: "width=device-width, initial-scale=1",
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
- },
-})
+
+  image: {
+    cloudinary: {
+      baseURL: "https://res.cloudinary.com/dckmlqzgd/image/upload/",
+    },
+    format: ["webp"],
+    loading: "lazy",
+  },
+
+  site: {
+    url: "https://zamann.netlify.app",
+    name: "Zaman Restaurant Nairobi",
+    description: "Zaman Restaurant Nairobi",
+    defaultLocale: "en",
+    identity: {
+      type: "Person",
+    },
+    twitter: "@mohamodjibril",
+    trailingSlash: true,
+  },
+  modules: ["@nuxt/image", "nuxt-icon", "@formkit/nuxt", "@nuxtseo/module"],
+
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
+});
